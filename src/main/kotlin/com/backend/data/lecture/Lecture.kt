1a42cb9d9b57546c009ca509bda7102cbf4bc39c
Sineha Manivannan <sineha.manivannan03@gmail.com>
2023-11-02 23:17:32 -0400

    sprint2 changed lec

--- src/main/kotlin/com/backend/data/lecture/Lecture.kt
+++ src/main/kotlin/com/backend/data/lecture/Lecture.kt
@@ -2,8 +2,13 @@ import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.types.ObjectId
 
 data class Lecture(
+    @BsonId val id: ObjectId = ObjectId(),
+
     val name: String,
     val active: Boolean,
-    @BsonId val id: ObjectId = ObjectId(),
-    val quizIds: Array<String> = emptyArray()
+    val studentIds: List<ObjectId>,
+    val teacherId: ObjectId,
+    val quizIds: List<ObjectId>,
+    val joinCode: String,
+    val isJoinable: Boolean
 )
